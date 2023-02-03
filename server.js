1c1a130eb1349cd7e2cf9dc53dd94dd66f9eeed8
David <5263778+Ffyud@users.noreply.github.com>
2023-02-03 15:33:00 +0100

    comments #2

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(express.json());
   
 app.use('/', express.static('public'));
 
-app.post('/user', async (req, res) => {
+
+// FIXME deze routes verplaatsen naar aparte route.js structuur
+
+app.post('/user', async (req, res) => { // een gebruiker toevoegen
   const new_user = User.build({ 
     username: req.body.username, 
     team: req.body.team
@@ -28,18 +31,24 @@ app.post('/user', async (req, res) => {
   res.send(req.body)
 });
 
-app.get('/user', async (req, res) => {
+app.get('/user', async (req, res) => { // alle gebruikers opvragen
   const all_users = await User.findAll();
   res.status(200)
   res.send(all_users)
 })
 
-app.get('/user/', async (req, res) => {
+app.get('/user/:id', async (req, res) => { // gebruiker met id opvragen
   const user = await User.findAll({
     where: {
-      id: 2
+      id: req.params.id
     }
   });
+  res.status(200)
+  res.send(user)
+})
+
+app.get('/question', async (req, res) => {
+  const user = await Question.findAll();
   res.status(200)
   res.send(user)
 })
