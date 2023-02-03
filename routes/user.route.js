b433cd2efaa9ed7b7d6a0d330f878510816cdc39
David <5263778+Ffyud@users.noreply.github.com>
2023-02-03 15:51:00 +0100

    move to routes #2

---
+++ routes/user.route.js
@@ -0,0 +1,37 @@
+import User from "../models/user.model.js";
+import express from "express";
+
+var userRouter = express.Router();
+
+await User.sync({ force: true }); // bij starten steeds schone tabel maken
+
+userRouter.post("/user", async (req, res) => {
+    // een gebruiker toevoegen
+    const new_user = User.build({
+        username: req.body.username,
+        team: req.body.team,
+    });
+    await new_user.save();
+    res.status(201);
+    res.send(req.body);
+});
+
+userRouter.get("/user", async (req, res) => {
+    // alle gebruikers opvragen
+    const all_users = await User.findAll();
+    res.status(200);
+    res.send(all_users);
+});
+
+userRouter.get("/user/:id", async (req, res) => {
+    // gebruiker met id opvragen
+    const user = await User.findAll({
+        where: {
+            id: req.params.id,
+        },
+    });
+    res.status(200);
+    res.send(user);
+});
+
+export default userRouter;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import User from './models/user.model.js'
+import userRouter from './routes/user.route.js';
 
 const app = express();
 const PORT = 8181;
@@ -9,8 +9,6 @@ var corsOptions = { // cross origin dingen
   origin: "http://localhost:8181"
 };
   
-await User.sync({ force: true }); // bij starten steeds schone tabel maken
-
 app.use(cors(corsOptions));
   
 // parse requests of content-type - application/json
@@ -18,34 +16,7 @@ app.use(express.json());
   
 app.use('/', express.static('public'));
 
-
-// FIXME deze routes verplaatsen naar aparte route.js structuur
-
-app.post('/user', async (req, res) => { // een gebruiker toevoegen
-  const new_user = User.build({ 
-    username: req.body.username, 
-    team: req.body.team
-  })
-  await new_user.save();
-  res.status(201)
-  res.send(req.body)
-});
-
-app.get('/user', async (req, res) => { // alle gebruikers opvragen
-  const all_users = await User.findAll();
-  res.status(200)
-  res.send(all_users)
-})
-
-app.get('/user/:id', async (req, res) => { // gebruiker met id opvragen
-  const user = await User.findAll({
-    where: {
-      id: req.params.id
-    }
-  });
-  res.status(200)
-  res.send(user)
-})
+app.use(userRouter)
 
 app.get('/question', async (req, res) => {
   const user = await Question.findAll();
