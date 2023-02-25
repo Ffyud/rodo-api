51d50a34c1c50a8e6b561fb46a8e0f0ae1fb6974
David <5263778+Ffyud@users.noreply.github.com>
2023-02-25 16:08:17 +0100

    voeg routes toe

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import userRouter from './routes/user.route.js';
+import questionlistRouter from './routes/questionlist.route.js';
+import questionRouter from './routes/question.route.js';
 
 const app = express();
 const PORT = 8181;
@@ -17,12 +19,8 @@ app.use(express.json());
 app.use('/', express.static('public'));
 
 app.use(userRouter)
-
-app.get('/question', async (req, res) => {
-  const user = await Question.findAll();
-  res.status(200)
-  res.send(user)
-})
+app.use(questionlistRouter)
+app.use(questionRouter)
 
 app.listen(PORT, () => {
     console.log(`De server doet het op ${PORT}`);
