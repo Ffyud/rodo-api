8ec1db22c4fc3e9743677ddf4cae4457d67b6f26
David <5263778+Ffyud@users.noreply.github.com>
2023-02-25 16:10:09 +0100

    voeg question routes toe

---
+++ routes/question.route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import Question from "../models/question.model.js";
+
+var questionRouter = express.Router();
+
+await Question.sync({ force: true }); // bij starten steeds schone tabel maken
+
+questionRouter.post("/question", async (req, res) => {
+    const new_question = Question.build({
+        content: req.body.content,
+        QuestionListId: req.body.questionlistId
+    });
+    await new_question.save();
+    res.status(201);
+    res.send(req.body);
+});
+
+questionRouter.get("/question", async (req, res) => {
+    // alle vragen opvragen
+    const all_questions = await Question.findAll();
+    res.status(200);
+    res.send(all_questions);
+});
+
+export default questionRouter;
